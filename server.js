d3671c4ab4c7401b7073c954d425a9f5844de3a9
vova <vova.makogonenko@gmail.com>
2024-10-12 14:41:19 +0400

    Удалил ненужный код в server.js

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 const express = require('express');
-const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
 const app = express();
 
-// Настраиваем multer для увеличения лимитов размера загружаемых файлов
-const upload = multer({
-  dest: 'uploads/',
-  limits: {
-    fileSize: 50 * 1024 * 1024, // Устанавливаем лимит на размер файла (50MB)
-  },
-});
 
 // Увеличиваем лимиты для JSON данных и URL-кодированных данных
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// API для загрузки изображений
-app.post('/upload', upload.single('image'), (req, res) => {
-  if (!req.file) return res.status(400).send('Нет файла');
-  res.send(`Файл загружен: ${req.file.filename}`);
-});
 
 // API для сохранения данных (стили, позиции и т.д.)
 app.post('/save-data', (req, res) => {
@@ -39,9 +26,6 @@ app.get('/get-data', (req, res) => {
     res.json(null); // Если данных нет, возвращаем null
   }
 });
-
-// Раздача статических файлов (загруженные изображения)
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Раздача административной панели (React)
 app.use('/admin', express.static(path.join(__dirname, 'maximus-client/build')));
